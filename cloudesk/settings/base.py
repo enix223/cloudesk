5dd0d05d5b6a3761d7caf9cd716831672a36c42a
Dell New <enix@cel.com>
2015-04-28 19:03:19 +0800

    add dev/prod settings file

--- cloudesk/settings/base.py
+++ cloudesk/settings/base.py
@@ -12,18 +12,6 @@ MANAGERS = ADMINS
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './cloudesk.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-}
-
 LOGIN_URL = 'login'
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
