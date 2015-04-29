afbc3ccde7364a037251c8c33870fe81ac525f4f
Dell New <enix@cel.com>
2015-04-29 09:11:43 +0800

    change production to use mysql db

--- cloudesk/settings/production.py
+++ cloudesk/settings/production.py
@@ -2,12 +2,11 @@ from base import *
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './cloudesk.db',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cloudesk',
+        'USER': 'cloudesk',
+        'PASSWORD': '123qwe',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
